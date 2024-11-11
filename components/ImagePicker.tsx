1ca47867cca022650a670b74330388253754cb30
Harshit Gupta <harshitgupta1406@gmail.com>
2024-11-11 17:14:43 +0530

    Allow only image uploads

--- components/ImagePicker.tsx
+++ components/ImagePicker.tsx
@@ -38,7 +38,7 @@ export default function CustomImagePicker() {
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 4],
       quality: 1,
