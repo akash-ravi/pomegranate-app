db5bc338207e4d0859f890b1e81220b7f3bf5a92
akash-ravi <akashravi28055@gmail.com>
2024-11-12 22:21:38 +0530

    model predict

--- components/ImagePicker.tsx
+++ components/ImagePicker.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useState } from 'react';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Image, View, StyleSheet, useColorScheme, Pressable } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
@@ -23,14 +22,60 @@ const initialImageState: ImageState = {
   filename: "",
 }
 
-const loadModel = async (): Promise<void | tf.LayersModel> => {
-  const model = await tf.loadLayersModel(
-    bundleResourceIO(modelJSON, modelWeights)
-  ).catch((e) => {
-    console.log("[LOADING ERROR] info:", e)
-  })
-  return model
+// const loadModel = async (): Promise<void | tf.LayersModel> => {
+//   const model = await tf.loadLayersModel(
+//     bundleResourceIO(modelJSON, modelWeights)
+//   ).catch((e) => {
+//     console.log("[LOADING ERROR] info:", e)
+//   })
+//   return model
+// }
+
+const loadModel = async()=>{
+  //.ts: const loadModel = async ():Promise<void|tf.LayersModel>=>{
+      const model = await tf.loadLayersModel(
+          bundleResourceIO(modelJSON, modelWeights)
+      ).catch((e)=>{
+        console.log("[LOADING ERROR] info:",e)
+      })
+      return model
+  }
+
+  const transformImageToTensor = async (uri)=>{
+    //.ts: const transformImageToTensor = async (uri:string):Promise<tf.Tensor>=>{
+    //read the image as base64
+      const img64 = await FileSystem.readAsStringAsync(uri, {encoding:FileSystem.EncodingType.Base64})
+      const imgBuffer =  tf.util.encodeString(img64, 'base64').buffer
+      const raw = new Uint8Array(imgBuffer)
+      let imgTensor = decodeJpeg(raw)
+      const scalar = tf.scalar(255)
+    //resize the image
+      imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [224, 224])
+    //normalize; if a normalization layer is in the model, this step can be skipped
+      const tensorScaled = imgTensor.div(scalar)
+    //final shape of the rensor
+      const img = tf.reshape(tensorScaled, [1,224,224,3])
+      return img
+  }
+
+
+const makePredictions = async ( batch, model, imagesTensor )=>{
+  //.ts: const makePredictions = async (batch:number, model:tf.LayersModel,imagesTensor:tf.Tensor<tf.Rank>):Promise<tf.Tensor<tf.Rank>[]>=>{
+  //cast output prediction to tensor
+  const predictionsdata= model.predict(imagesTensor)
+  //.ts: const predictionsdata:tf.Tensor = model.predict(imagesTensor) as tf.Tensor
+  let pred = predictionsdata.split(batch) //split by batch size
+  //return predictions 
+  return pred
 }
+
+  export const getPredictions = async (image)=>{
+    await tf.ready()
+    const model = await loadModel() as tf.LayersModel
+    const tensor_image = await transformImageToTensor(image)
+    const predictions = await makePredictions(1, model, tensor_image)
+    return predictions    
+  }
 
 export default function CustomImagePicker() {
   const [image, setImage] = useState<ImageState>(initialImageState);
@@ -88,6 +133,9 @@ export default function CustomImagePicker() {
     const db = connectToDatabase();
     await tf.ready()
     const model = loadModel()
+    const tensorImage = await transformImageToTensor(image);
+    const predictions = await makePredictions(1, model, tensorImage);
+    console.log(predictions);
     const imageRecord: ImageType = {
       imagePath: imagePath,
       type: "bacterial",
@@ -128,8 +176,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 224,
+    height: 224,
   },
   pickContainer: {
     padding: 10,
